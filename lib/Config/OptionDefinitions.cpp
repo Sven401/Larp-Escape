fc1420281f5110183d2618818b151062ff7c047b
Sebastian Seidel <s.seidel@energieversum.de>
2025-09-23 20:24:36 +0200

    Position von Button 3 in pair 1 auf H 2 gelegt, da E9 nicht belegt ist, Typo fix

--- lib/Config/OptionDefinitions.cpp
+++ lib/Config/OptionDefinitions.cpp
@@ -12,27 +12,27 @@ std::vector<OptionConfig> getDefaultOptions()
         OptionConfig(UID1, "Quail",
                      {
                          // loggingsymbol, loggingcategory, color ,row, col, audioFile
-                         ButtonPair("undefined", "undefined", &Blue_p, 7, B, 101),
-                         ButtonPair("undefined", "undefined", &Blue_p, 8, H, 103),
-                         ButtonPair("undefined", "undefined", &Blue_p, 9, E, 109),
+                         ButtonPair("Button1", "Pair1", &Blue_p, 7, B, 101), 
+                         ButtonPair("Button2", "Pair1", &Blue_p, 8, H, 103), 
+                         ButtonPair("Button3", "Pair1", &Blue_p, 2, H, 109),
                      },
-                     500), // generic solved sound)
+                     110), // generic solved sound)
         OptionConfig(UID2, "SecRinsui",
                      {
                          // loggingsymbol, loggingcategory, color ,row, col, audioFile
-                         ButtonPair("undefined", "undefined", &Red_p, 2, C, 204),
-                         ButtonPair("undefined", "undefined", &Red_p, 5, A, 206),
-                         ButtonPair("undefined", "undefined", &Red_p, 4, I, 207),
+                         ButtonPair("Button1", "Pair2", &Red_p, 2, C, 204),
+                         ButtonPair("Button2", "Pair2", &Red_p, 5, A, 206),
+                         ButtonPair("undefined", "Pair2", &Red_p, 4, I, 207),
                      },
-                     500), // generic solved sound)
+                     210), // generic solved sound)
         OptionConfig(UID3, "|||",
                      {
                          // loggingsymbol, loggingcategory, color ,row, col, audioFile
-                         ButtonPair("undefined", "undefined", &Yellow_p, 8, F, 302),
-                         ButtonPair("undefined", "undefined", &Yellow_p, 6, G, 305),
-                         ButtonPair("undefined", "undefined", &Yellow_p, 1, D, 308),
+                         ButtonPair("Button1", "Pair3", &Yellow_p, 8, F, 302),
+                         ButtonPair("Button2", "Pair3", &Yellow_p, 6, G, 305),
+                         ButtonPair("Button3", "Pair3", &Yellow_p, 1, D, 308),
                      },
-                     500), // generic solved sound)
+                     310), // generic solved sound)
 
     };
 }
